8099b93ffb3fa2ff73fa34b3624cd9411106c9e7
heyp <heyp@list-dev.intra.camosun.bc.ca>
2018-03-15 15:42:54 -0700

    changes to view, uses render instead.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
 
 from .models import Question
 
@@ -8,11 +8,10 @@ from .models import Question
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = {
-		'latest_question_list': latest_question_list,
-	}
-	return HttpResponse(template.render(context, request))
+#	template = loader.get_template('polls/index.html')
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+#	return HttpResponse(template.render(context, request))
 #	output = ', '.join([q.question_text for q in latest_question_list])
 #	return HttpResponse(output)
 #	return HttpResponse("Hello, World! You're at the polls index.")
